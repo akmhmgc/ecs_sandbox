fc4255b54f0ae3d3ff7061a669a2c3d9bee0392f
akmhmgc <akimuhamaguchi@icloud.com>
2024-08-26 08:52:11 +0900

    add ecs

---
+++ ecs.tf
@@ -0,0 +1,92 @@
+resource "aws_ecs_cluster" "main" {
+  name = "nginx_cluster"
+}
+
+resource "aws_iam_role" "ecs_task_exec" {
+  name = "ecs_task_exec_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = { Service = "ecs-tasks.amazonaws.com" }
+        Action    = "sts:AssumeRole"
+      }
+    ]
+  })
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  ]
+}
+
+resource "aws_ecs_task_definition" "nginx_task" {
+  family                   = "nginx_task"
+  network_mode             = "awsvpc"
+  execution_role_arn       = aws_iam_role.ecs_task_exec.arn
+  requires_compatibilities = ["EC2"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([
+    {
+      name  = "nginx"
+      image = "nginx:latest"
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group" = aws_cloudwatch_log_group.nginx_task.name
+          "awslogs-region" : "ap-northeast-1",
+          "awslogs-stream-prefix" : "ecs"
+        }
+      }
+    }
+  ])
+}
+
+resource "aws_security_group" "ecs_http_sg" {
+  vpc_id = aws_vpc.main.id
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "ecs_instance_sg"
+  }
+}
+
+resource "aws_ecs_service" "nginx_service" {
+  name            = "nginx_service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.nginx_task.arn
+  desired_count   = 2
+  launch_type     = "EC2"
+  network_configuration {
+    subnets         = [aws_subnet.public.id]
+    security_groups = [aws_security_group.ecs_http_sg.id]
+  }
+  depends_on = [aws_lb_target_group.nginx_tg]
+  load_balancer {
+    target_group_arn = aws_lb_target_group.nginx_tg.arn
+    container_name   = "nginx"
+    container_port   = 80
+  }
+}
+
+resource "aws_cloudwatch_log_group" "nginx_task" {
+  name              = "/ecs/kafka_trial_task"
+  retention_in_days = 7
+}
