702b0ef2d4cb5b04f7c471e7e56560fabd45e714
akmhmgc <akimuhamaguchi@icloud.com>
2024-08-26 08:51:32 +0900

    feat: add alb

---
+++ alb.tf
@@ -0,0 +1,47 @@
+resource "aws_security_group" "nginx_sg" {
+  vpc_id = aws_vpc.main.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "nginx_sg"
+  }
+}
+
+resource "aws_lb" "nginx_lb" {
+  name               = "nginx-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.nginx_sg.id]
+  subnets            = [
+    aws_subnet.public.id,
+    aws_subnet.sub.id
+    ]
+}
+
+resource "aws_lb_target_group" "nginx_tg" {
+  name     = "nginx-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+  target_type = "ip"
+}
+
+resource "aws_lb_listener" "nginx_listener" {
+  load_balancer_arn = aws_lb.nginx_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.nginx_tg.arn
+  }
+}
