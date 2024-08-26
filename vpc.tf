dc1bdf8c620e238a4a930e872a9d01622ef24c72
akmhmgc <akimuhamaguchi@icloud.com>
2024-08-27 06:58:21 +0900

    feat: make ssm manager connect with private link

--- vpc.tf
+++ vpc.tf
@@ -45,3 +45,74 @@ resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
+
+
+# セッションマネージャー用のprivate vpc endpoint
+data "aws_iam_policy_document" "vpc_endpoint" {
+  statement {
+    effect    = "Allow"
+    actions   = ["*"]
+    resources = ["*"]
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+  }
+}
+
+resource "aws_vpc_endpoint" "ssm" {
+  vpc_endpoint_type = "Interface"
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.ap-northeast-1.ssm"
+  policy            = data.aws_iam_policy_document.vpc_endpoint.json
+  subnet_ids = [
+    aws_subnet.sub.id
+  ]
+  private_dns_enabled = true
+  security_group_ids = [
+    aws_security_group.ssm.id
+  ]
+}
+
+resource "aws_vpc_endpoint" "ssmmessages" {
+  vpc_endpoint_type = "Interface"
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.ap-northeast-1.ssmmessages"
+  policy            = data.aws_iam_policy_document.vpc_endpoint.json
+  subnet_ids = [
+    aws_subnet.sub.id
+  ]
+  private_dns_enabled = true
+  security_group_ids = [
+    aws_security_group.ssm.id
+  ]
+}
+
+resource "aws_vpc_endpoint" "ec2messages" {
+  vpc_endpoint_type = "Interface"
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.ap-northeast-1.ec2messages"
+  policy            = data.aws_iam_policy_document.vpc_endpoint.json
+  subnet_ids = [
+    aws_subnet.sub.id
+  ]
+  private_dns_enabled = true
+  security_group_ids = [
+    aws_security_group.ssm.id
+  ]
+}
+resource "aws_security_group" "ssm" {
+  name        = "ssm-sg"
+  description = "ssm-sg"
+  vpc_id      = aws_vpc.main.id
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+
+  tags = {
+    Name = "ssm-sg"
+  }
+}
