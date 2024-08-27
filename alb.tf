2c96edfbca0d9f2b6b6a0f135250b4f8e1355d08
akmhmgc <akimuhamaguchi@icloud.com>
2024-08-27 19:14:36 +0900

    feat: execute ecs task in bridge network mode

--- alb.tf
+++ alb.tf
@@ -33,7 +33,7 @@ resource "aws_lb_target_group" "nginx_tg" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
-  target_type = "ip"
+  target_type = "instance"
 }
 
 resource "aws_lb_listener" "nginx_listener" {
